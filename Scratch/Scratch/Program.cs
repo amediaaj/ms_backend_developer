d5f20e3f4ff9b39015c9f6fc3a65be105b46c83d
Alex Amedia <aamedia@asu.edu>
2024-12-24 12:32:27 -0700

    scratch - Data Type Conversion

--- Scratch/Scratch/Program.cs
+++ Scratch/Scratch/Program.cs
@@ -1,41 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Explicit conversion (casting)
+double piVar = 3.14;
+int piInt = (int)piVar;
+Console.WriteLine(piInt);
 
-// Check type
-string myVariable = "This fruit is an apple.";
-Type dataType = myVariable.GetType();
-Console.WriteLine(dataType);
-
-
-// Assigning an integer to variable of type obj
-object myObjVariable = 123;
-if(myObjVariable.GetType() == typeof(int)){
-    Console.WriteLine($"The variable {myObjVariable} is an integer.");
-} else
-{
-    Console.WriteLine($"The variable {myObjVariable} is NOT an integer");
-}
-
-// Assigning a string to the same variable of type obj
-myObjVariable = "123";
-if(myObjVariable.GetType() == typeof(int)){
-    Console.WriteLine($"The variable {myObjVariable} is an integer.");
-} else
-{
-    Console.WriteLine($"The variable {myObjVariable} is NOT an integer");
-}
-
-// Parse string to integer
-myVariable = "123";
-int number = int.Parse(myVariable);
-Console.WriteLine(number + 1);
-
-
-// Try parse string to integer
-if(int.TryParse(myVariable, out int parsed_number))
-{
-    Console.WriteLine($"Conversion successful: {parsed_number}");
-} else
-{
-    Console.WriteLine($"Conversion Failed");
-}
-Console.WriteLine(parsed_number);
+// Explicit conversion (parsing)
+string myString = "123";
+int myInt = int.Parse(myString);
+Console.WriteLine(myInt);
