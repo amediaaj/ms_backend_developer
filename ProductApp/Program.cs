5f5cef9f92cbbcb66e1f4ebe61f2dce44ec3e7b4
Alex Amedia <aamedia@asu.edu>
2025-01-02 13:53:43 -0700

    ProductApp - JSON Serialize/Deserialize

---
+++ ProductApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }  
+    public List<string> Tags { get; set; }  
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+      string json = "{\"Name\":\"Apple\",\"Price\":3.99,\"Tags\":[\"Fruit\",\"Organic\"]}";
+      Product price = JsonConvert.DeserializeObject<Product>(json);
+      Console.WriteLine($"Name: {price.Name}, Price: {price.Price}, Tags: {string.Join(", ", price.Tags)}");  
+
+      Product newProduct = new Product
+      {
+          Name = "Banana",
+          Price = 2.99m,
+          Tags = new List<string> { "Fruit", "Organic" }
+      };
+      Console.WriteLine(JsonConvert.SerializeObject(newProduct));
+
+    }
+}
