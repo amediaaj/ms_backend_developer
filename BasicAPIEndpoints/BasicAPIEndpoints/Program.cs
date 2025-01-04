e25fff709bb031d6dfe80dbc2f4ff3c6a3c55e57
Alex Amedia <aamedia@asu.edu>
2025-01-04 10:56:29 -0700

    basic_api_endpoints - Implementing Basic API Endpoints

--- BasicAPIEndpoints/BasicAPIEndpoints/Program.cs
+++ BasicAPIEndpoints/BasicAPIEndpoints/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Root Path");
+app.MapGet("/Downloads", () => "Downloads");
+app.MapPut("/", () => "This is a put request");
+app.MapDelete("/", () => "This is a delete request");
+app.MapPost("/", () => "This is a post request");
 
 app.Run();
