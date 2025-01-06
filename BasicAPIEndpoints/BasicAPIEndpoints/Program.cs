e465c1b2f92a4f35be4181cd7083422c3e179929
Alex Amedia <aamedia@asu.edu>
2025-01-06 12:10:20 -0700

    Advanced Routing Techniques

--- BasicAPIEndpoints/BasicAPIEndpoints/Program.cs
+++ BasicAPIEndpoints/BasicAPIEndpoints/Program.cs
@@ -3,14 +3,36 @@ var app = builder.Build();
 
 // Static routes
 app.MapGet("/", () => "Root Path");
-app.MapGet("/Users", () => "Users");
-app.MapGet("/Downloads", () => "Downloads");
+app.MapGet("/users", () => "Users");
+app.MapGet("/downloads", () => "Downloads");
 app.MapPut("/", () => "This is a put request");
 app.MapDelete("/", () => "This is a delete request");
 app.MapPost("/", () => "This is a post request");
 
 // Routes with parameters
-app.MapGet("/Users/{id:int}", (int id) => $"User ID: {id}");
-app.MapGet("/Names/{id}", (string id) => $"Name ID: {id}");
-app.MapGet("/Products/{id?}", (int? id=99) => $"Product ID: {id}");
+app.MapGet("/users/{id:int}", (int id) => $"User ID: {id}");
+app.MapGet("/names/{id}", (string id) => $"Name ID: {id}");
+// min id value
+app.MapGet("/products/{id::int:min(100)}", (int id) => $"Product ID: {id}");
+// Handles string parameter
+// app.MapGet("/products/{id}", (string id) => $"Product ID: {id}");
+// Handles optional parameter
+app.MapGet("/products/{id:int?}", (int? id=99) => $"Product ID: {id}");
+
+// More complex routes
+app.MapGet("/users/{id}/posts/{slug}", (string id, string slug) => {
+    return $"User ID: {id} Post ID: {slug}";
+    });
+
+// Catchall route
+app.MapGet("/files/{*filepath}", (string filepath) => {
+    return $"File Path: {filepath}";
+    });
+
+// Route with query string parameters
+app.MapGet("/search", (string? q, int page = 1) => {
+    return $"Searching for: {q} on Page: {page}";
+    });
+
 app.Run();
+
